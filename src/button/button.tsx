e264652fdc85f096d9bc108465e694c62aad99f7
Albert Willians <albertwillians@hotmail.com>
2021-12-14 11:40:03 -0300

    deletando arquivos inuteis e definindo a base para a lib de components

---
+++ src/button/button.tsx
@@ -0,0 +1,97 @@
+import { ElementType, MouseEventHandler, ReactNode } from 'react';
+
+export type ButtonType = 'default' | 'danger' | 'ghost' | 'secondary';
+
+export type ComponentSize = 'default' | 'large' | 'small';
+
+interface BaseButtonProps {
+   type?: ButtonType;
+   icon?: ElementType;
+   size?: ComponentSize;
+   className?: string;
+   children?: ReactNode;
+   disabled?: boolean;
+   loading?: boolean;
+}
+
+type HTMLButtonProps = {
+   onClick?: MouseEventHandler<HTMLButtonElement>;
+} & BaseButtonProps;
+
+type HTMLAnchorProps = {
+   href?: string;
+} & BaseButtonProps;
+
+/**
+ *  For React Router Link
+ */
+type CustomNodeProps = {
+   as?: ElementType;
+   to?: string;
+} & BaseButtonProps;
+
+export type ButtonProps = HTMLButtonProps & HTMLAnchorProps & CustomNodeProps;
+
+const Button: React.ForwardRefRenderFunction<unknown, ButtonProps> = (props, ref) => {
+   const {
+      type = 'default',
+      icon,
+      size = 'default',
+      className = '',
+      children,
+      disabled = false,
+      loading = false,
+      onClick,
+      href,
+      as,
+      to
+   } = props;
+
+   const styles = {
+      innerType: type,
+      size,
+      disabled,
+      withText: children !== null
+   }
+
+   if (as) {
+      return (
+         <StyledButton
+            as={as}
+            to={to}
+            ref={ref}
+            className={className}
+            {...styles}
+         >
+            {loading ? 'Loading...' : children}
+         </StyledButton>
+      )
+   }
+
+   if (href) {
+      return (
+         <StyledButton
+            as='a'
+            href={href}
+            ref={ref}
+            className={className}
+            {...styles}
+         >
+            {loading ? 'Loading...' : children}
+         </StyledButton>
+      )
+   }
+
+   return (
+      <StyledButton
+         as='button'
+         type='button'
+         onClick={onClick}
+         ref={ref}
+         className={className}
+         {...styles}
+      >
+         {loading ? 'Loading...' : children}
+      </StyledButton>
+   )
+}
